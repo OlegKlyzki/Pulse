f3e6acc679e2cacf9ee103811e6b82fec4934a0c
OlegKlyzki <olegausss368@gmail.com>
2021-12-21 10:55:37 +0200

    Pulse Full + Modal

--- src/js/script.js
+++ src/js/script.js
@@ -73,7 +73,8 @@ $(document).ready(function(){
 				$(this).on('click', function(e) {
 					e.preventDefault();
 					$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-					$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+					$('.catalog-item__menu .catalog-item__list').eq(i).toggleClass('catalog-item__menu_active .catalog-item__list_active');
+					$('.catalog-item__back').eq(i).toggleClass('catalog-item__back_active');
 				});
 			});
 		}
@@ -158,7 +159,5 @@ $(document).ready(function(){
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
     	});
-
-		new WOW().init();
 	});
 	})(jQuery);
